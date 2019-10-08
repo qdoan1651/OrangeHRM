b53495cb86552ba33a21de9885fe541eb74b8639
Quyen Doan <qdoan1651@gmail.com>
2019-10-08 15:34:37 -0800

    Added page object locators

---
+++ locators/locators.py
@@ -0,0 +1,16 @@
+'''
+@author: Quyen Doan
+https://github.com/qdoan1651/OrangeHRM/locators/locators.py
+'''
+class Locators:
+    # login page objects
+    username_textbox_id = 'txtUsername'
+    password_textbox_id = 'txtPassword'
+    login_button_id = 'btnLogin'
+    
+    # home page objects
+    welcome_link_id = 'welcome'
+    logout_link_linktext = 'Logout'
+    logout_link_xpath = '//div[@id="welcome-menu"]/ul/li[2]/a'
+    
+    
