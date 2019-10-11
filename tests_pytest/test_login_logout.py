9d558f04db63936b3cd41e7a9555ccf50cfe09a8
Quyen Doan <qdoan1651@gmail.com>
2019-10-11 07:12:48 -0800

    Removed class TestOrangeHRM and pushed tests to top level

--- tests_pytest/test_login_logout.py
+++ tests_pytest/test_login_logout.py
@@ -10,28 +10,27 @@ sys.path.append(project_path)
 from pages.login_page import LoginPage
 from pages.home_page import HomePage 
 
-class TestOrangeHRM():
-    @pytest.fixture()
-    def test_setup(self):
-        global driver
-        project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-        chrome_path = os.path.join(project_path, 'drivers/chromedriver.v.77.exe')
-        driver = webdriver.Chrome(chrome_path)
-        driver.set_window_size(1600, 900)
-        yield
-        driver.close()
-        driver.quit()
-    
-    def test_login(self, test_setup):
-        # login
-        login_page = LoginPage(driver)
-        login_page.login_as_admin()
-    
-    def test_logout(self, test_setup):
-        # login
-        login_page = LoginPage(driver)
-        login_page.login_as_admin()
- 
-        # log out
-        home_page = HomePage(driver)
-        home_page.logout()
+@pytest.fixture()
+def test_setup():
+    global driver
+    project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    chrome_path = os.path.join(project_path, 'drivers/chromedriver.v.77.exe')
+    driver = webdriver.Chrome(chrome_path)
+    driver.set_window_size(1600, 900)
+    yield
+    driver.close()
+    driver.quit()
+
+def test_login(test_setup):
+    # login
+    login_page = LoginPage(driver)
+    login_page.login_as_admin()
+
+def test_logout(test_setup):
+    # login
+    login_page = LoginPage(driver)
+    login_page.login_as_admin()
+
+    # log out
+    home_page = HomePage(driver)
+    home_page.logout()
