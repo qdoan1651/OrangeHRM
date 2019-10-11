3ab3127d19fbe1c173b4410937d90a39e5398d09
Quyen Doan <qdoan1651@gmail.com>
2019-10-11 07:13:25 -0800

    Added new locators

--- locators/locators.py
+++ locators/locators.py
@@ -12,5 +12,18 @@ class Locators:
     welcome_link_id = 'welcome'
     logout_link_linktext = 'Logout'
     logout_link_xpath = '//div[@id="welcome-menu"]/ul/li[2]/a'
+
+    admin_tab_id = 'menu_admin_viewAdminModule'
+    user_management_tab_id = 'menu_admin_UserManagement'
     
+    pim_tab_id = 'menu_pim_viewPimModule'
+    employee_list_tab_id = 'menu_pim_viewEmployeeList'
     
+    leave_tab_id = 'menu_leave_viewLeaveModule'
+    leave_list_tab_id = 'menu_leave_viewLeaveList'
+    
+    time_tab_id = 'menu_time_viewTimeModule'
+    define_time_sheet_id = 'defineTimesheet'
+    
+    recruitment_tab_id = 'menu_recruitment_viewRecruitmentModule'
+    candidates_tab_id = 'menu_recruitment_viewCandidates'
