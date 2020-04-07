67044eb7aab64a47b9fbfdf846ed43586cadd4ad
Quyen Doan <qdoan1651@gmail.com>
2020-04-07 11:30:22 -0800

    Updated chrome driver to version 80

--- tests_pytest/test_admin_functions.py
+++ tests_pytest/test_admin_functions.py
@@ -14,7 +14,7 @@ from pages.home_page import HomePage
 def test_setup():
     global driver
     project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    chrome_path = os.path.join(project_path, 'drivers/chromedriver.v.77.exe')
+    chrome_path = os.path.join(project_path, 'drivers/chromedriver.v.80.exe')
     driver = webdriver.Chrome(chrome_path)
     driver.set_window_size(1600, 900)
     login_page = LoginPage(driver)
@@ -26,20 +26,25 @@ def test_setup():
 def test_click_admin_tab(test_setup):
     homepage = HomePage(driver)
     homepage.click_admin_tab()
-
+    assert homepage.get_management_tab_text() == 'User Management'
+    
 def test_click_pim_tab(test_setup):
     homepage = HomePage(driver)
     homepage.click_pim_tab()
+    assert homepage.get_employee_list_tab_text() == 'Employee List'
  
 def test_click_leave_tab(test_setup):
     homepage = HomePage(driver)
     homepage.click_leave_tab()
+    assert homepage.get_leave_list_tab_text() == 'Leave List'
  
 def test_click_time_tab(test_setup):
     homepage = HomePage(driver)
     homepage.click_time_tab()
+    assert homepage.get_timesheets_tab_text() == 'Timesheets'
  
 def test_click_recruitment_tab(test_setup):
     homepage = HomePage(driver)
     homepage.click_recruitment_tab()
- 
+    assert homepage.get_candidates_tab_text() == 'Candidates'
+
